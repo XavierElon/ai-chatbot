d67bd3897c2284b21648d531aad232f21ccc5719
Xavier <xavierh93@hotmail.com>
2024-08-05 19:45:57 -0400

    create pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import DashboardPage from './routes/dashboardPage/DashboardPage'
+import Homepage from './routes/homepage/Homepage'
+import ChatPage from './routes/chatPage/Chatpage'
+import SignInPage from './routes/signInPage/SignInPage'
+import SignUpPage from './routes/signUpPage/SignUpPage'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />
+  },
+  {
+    path: '/dashboard',
+    
+    children: [
+      { path: '/dashboard', element: <DashboardPage /> },
+      { path: '/dashboard/chats/:id', element: <ChatPage /> }
+    ]
+  },
+  {
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
