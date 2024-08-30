394fd42e30de07a28c738f5180a3c6f826382024
XavierElon <xavierelon93@gmail.com>
2024-08-30 17:08:40 -0400

    styling

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom'
 const Homepage = () => {
   return (
     <div className="homepage">
-      <Link to="/dashboard">Dashboard</Link>
+      <div className="left">
+        <h1>Xavier Elon AI Chat Bot</h1>
+        <h2>Test</h2>
+        <h3>Cosmo</h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
