a2db71c2c58887d1b5b8748a7abc18e892c9c2db
Xavier <xavierh93@hotmail.com>
2024-09-11 17:27:38 -0400

    dashboard chat styling

--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -2,37 +2,37 @@ import './dashboardPage.css'
 
 const DashboardPage = () => {
   return (
-    <div className='dashboardPage'>
+    <div className="dashboardPage">
       <div className="texts">
         <div className="logo">
           <img src="/logo.png" alt="" />
-          <h1>Xavier Elon AI Chat Bot</h1>
+          <h1>LAMA AI</h1>
         </div>
         <div className="options">
           <div className="option">
-            <img src='/chat.png' alt='' />
+            <img src="/chat.png" alt="" />
             <span>Create a New Chat</span>
           </div>
           <div className="option">
-            <img src='/image.png' alt='' />
+            <img src="/image.png" alt="" />
             <span>Analyze Images</span>
           </div>
           <div className="option">
-            <img src='/code.png' alt='' />
-            <span>Help me with my Code</span>
+            <img src="/code.png" alt="" />
+            <span>Help Me Code</span>
           </div>
         </div>
       </div>
       <div className="formContainer">
         <form>
-          <input type='text' placeholder='Ask me anything' />
+          <input type="text" name="text" placeholder="Ask me anything..." />
           <button>
-            <img src='/arrow.png' alt='' />
+            <img src="/arrow.png" alt="" />
           </button>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
 export default DashboardPage
