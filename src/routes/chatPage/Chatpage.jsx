9bac4d19070998d33d07004edcc4e9e0689f09b1
Xavier <xavierh93@hotmail.com>
2024-09-11 18:29:58 -0400

    working on chatpage styling

--- src/routes/chatPage/Chatpage.jsx
+++ src/routes/chatPage/Chatpage.jsx
@@ -2,7 +2,36 @@ import './chatpage.css'
 
 const Chatpage = () => {
   return (
-    <div className='chatpage'>Chatpage</div>
+    <div className='chatpage'>
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from UserNostrud ea dolore aliquip sunt do cillum Lorem amet deserunt ut.</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
