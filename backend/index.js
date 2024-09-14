10d8b502cd9e1e99d9aaa87d8224eec00370f4be
Xavier <xavierh93@hotmail.com>
2024-09-13 20:38:43 -0400

    setup express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log('test')
+import express from 'express'
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app.get('/test/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
