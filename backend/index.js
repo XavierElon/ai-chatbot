8eb7648d0ac67c61363d9baa0a22981abd2ff11a
Xavier <xavierh93@hotmail.com>
2024-09-24 18:23:05 -0400

    image kit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import ImageKit from 'imagekit'
 
 const port = process.env.PORT || 3000
 const app = express()
 
-app.get('/test/', (req, res) => {
-    res.send('Hello World!')
+const imagekit = new ImageKit({
+    urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY
+  });
+
+app.get('/api/upload', (req, res) => {
+    const result = imagekit.getAuthenticationParameters();
+    res.send(result)
 })
 
 app.listen(port, () => {
